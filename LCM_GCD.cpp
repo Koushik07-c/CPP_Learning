da492d9b9721faa8325e41ec6cbc62f72d1f3f1b
Koushik07-c <koushikkite07@gmail.com>
2026-03-04 21:48:09 +0530

    Update LCM_GCD.cpp

--- LCM_GCD.cpp
+++ LCM_GCD.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;
 
+// Function to find GCD
+int findGCD(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+// Function to find LCM
+int findLCM(int a, int b) {
+    return (a * b) / findGCD(a, b);
+}
+
+int main() {
+    int num1, num2;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    int gcd = findGCD(num1, num2);
+    int lcm = findLCM(num1, num2);
+
+    cout << "GCD = " << gcd << endl;
+    cout << "LCM = " << lcm << endl;
+
+    return 0;
+}
